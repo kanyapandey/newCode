2ccec8e7627899af6c54d1c9f780a49775ba212a
Pandey <kanya@kanyas-MacBook-Pro.local>
2015-12-15 23:30:34 +0700

    Initial Commit

---
+++ D'Predictor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D'Predictor
+//
+//  Created by kanya pandey on 12/15/2558 BE.
+//  Copyright © 2558 kanya pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
